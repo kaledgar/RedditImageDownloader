3a5f9b490e9bf5094155a72cc80f118d73f00641
kkinastowski66 <kacperlucas672@gmail.com>
2023-05-25 19:16:35 +0200

    added requirements.txt

--- Reddit Image Downloader/__main__.py
+++ Reddit Image Downloader/__main__.py
@@ -1,10 +1,13 @@
 from image_downloader_module import *
 
 #test for multiple media in one submission
-name = 'your_user_name'
+#name1 = 'vegetaaaa88'
+name = 'YuzuPyon'
 
 rid = RedditImageDownload(name)
 df = rid._get_posts()
 df1 = rid.classify_urls(df)
 
 print(df1.to_string())
+
+rid.get_images(name_by='id')
