bf113fea4f05a847e5719b182f12e00a4c790793
kkinastowski66 <kacperlucas672@gmail.com>
2023-11-12 22:25:57 +0100

    major refacotr, added typing and common error handling, removed unnecessary code

--- reddit_image_downloader/constants.py
+++ reddit_image_downloader/constants.py
@@ -1,6 +1,8 @@
 DEFAULT_CREDENTIALS_FILEPATH = "user_credentials.json"
+DEFAULT_DOWNLOADS_PATH = "./downloads"
+DEFAULT_IMAGE_FORMAT = "jpg"
 DEFAULT_POST_COUNT = 10
 DEFAULT_USER_NAMES = [
-    #very cool fantasy illustrator artist
+    # very cool fantasy illustrator artist
     "Phill-Art"
 ]
--- reddit_image_downloader/reddit_image_downloader.py
+++ reddit_image_downloader/reddit_image_downloader.py
@@ -2,27 +2,58 @@ import json
 import logging
 import os
 from datetime import datetime
-
+from typing import Optional, List, Tuple
 import pandas as pd
 import praw
 import requests
 
-## add error handling!!!!!!!
 from prawcore.exceptions import Forbidden
 
-from .constants import DEFAULT_CREDENTIALS_FILEPATH, DEFAULT_POST_COUNT
+from .constants import (
+    DEFAULT_CREDENTIALS_FILEPATH,
+    DEFAULT_DOWNLOADS_PATH,
+    DEFAULT_IMAGE_FORMAT,
+)
 
 logger = logging.getLogger(__name__)
 
 
 class RedditImageDownloader:
-    def __init__(self, user_name, credentials_filepath=DEFAULT_CREDENTIALS_FILEPATH):
+    def __init__(
+        self,
+        user_name: str,
+        credentials_filepath: str = DEFAULT_CREDENTIALS_FILEPATH,
+        downloads_path: str = DEFAULT_DOWNLOADS_PATH,
+    ) -> None:
+        """
+        Initialize RedditImageDownloader. Images will be downloaded to:
+            /downloads_path/user_name
+
+        Args:
+            user_name (str): Reddit username.
+            credentials_filepath (str, optional): Path to the credentials file. Defaults to DEFAULT_CREDENTIALS_FILEPATH.
+            downloads_path (str, optional) : Parent directory where images will be downloaded
+        """
         self.user_name = user_name
+
+        self.downloads_path = downloads_path
+        self.user_path = f"{self.downloads_path}/{self.user_name}"
+
+        self._make_directory()
         self.reddit = self._get_reddit_from_filepath(credentials_filepath)
         self.user = self.reddit.redditor(self.user_name)
 
     @staticmethod
-    def _get_reddit_from_filepath(credentials_filepath):
+    def _get_reddit_from_filepath(credentials_filepath) -> praw.Reddit:
+        """
+        Load Reddit instance from credentials file.
+
+        Args:
+            credentials_filepath (str): Path to the credentials file.
+
+        Returns:
+            praw.Reddit: Initialized Reddit instance.
+        """
         with open(credentials_filepath, "r") as file:
             credentials = json.load(file)
         return praw.Reddit(
@@ -33,14 +64,27 @@ class RedditImageDownloader:
             username=credentials["username"],
         )
 
-    def _make_directory(self):
-        path = f"./{self.user_name}"
-        if not os.path.exists(path):
-            os.mkdir(path)
-        else:
-            pass
+    def _make_directory(self) -> None:
+        """
+        Create a directory for the user if it doesn't exist.
+        """
+        if not os.path.exists(self.downloads_path):
+            os.mkdir(self.downloads_path)
+        if not os.path.exists(self.user_path):
+            os.mkdir(self.user_path)
 
-    def _test_none_type_submission(self, gallery_id):
+    def _check_none_type_submission(
+        self, gallery_id: str
+    ) -> Optional[praw.models.Submission]:
+        """
+        Check if the submission is of None.
+
+        Args:
+            gallery_id (str): ID of the submission.
+
+        Returns:
+            Optional[praw.models.Submission]: Submission instance if it exists, None otherwise.
+        """
         post = self.reddit.submission(id=gallery_id)
         if post.author is None:
             logger.error(f"Submission with ID {gallery_id} was deleted.")
@@ -52,7 +96,16 @@ class RedditImageDownloader:
             logger.info(f"Submission with ID {gallery_id} is UP.")
             return post
 
-    def _get_posts(self, posts_count=None):
+    def _get_posts(self, posts_count: int = None) -> pd.DataFrame:
+        """
+        Returns DataFrame with metadata from obtained posts.
+
+        Args:
+            posts_count (int): limits max number of metadata loaded to dataframe
+
+        Returns:
+            pd.DataFrame: DataFrame with posts metadata (id, url, created_utc)
+        """
         user_submissions = [
             (subm.id, subm.url, subm.created_utc)
             for subm in self.user.submissions.new(limit=posts_count)
@@ -61,24 +114,15 @@ class RedditImageDownloader:
         logger.debug(f"{self.user_name} posts: {df}")
         return df
 
-    def _get_posts_from_profile(self, posts_count=DEFAULT_POST_COUNT):
-        profile = self.user.subreddit.display_name
-        subreddit = self.reddit.subreddit(profile)
-        post_info = [
-            (
-                subm.id,
-                str(subm.url),
-                str(subm.title),
-                int(subm.created_utc),
-                subm.subreddit,
-            )
-            for subm in subreddit.top(limit=posts_count)
-        ]
-
-        df = pd.DataFrame(post_info, columns=["id", "url", "title", "created_utc"])
-        return df
-
     def classify_urls(self, df):
+        """_summary_
+
+        Args:
+            df (pd.DataFrame): classify URLs in metadata df
+
+        Returns:
+            df : dataframe with metadata
+        """
         df["type"] = df["url"].apply(
             lambda x: "image"
             if "i.redd.it" in x
@@ -90,57 +134,73 @@ class RedditImageDownloader:
             if "v.redd.it" in x
             else "unknown"
         )
-        logger.info(df)
         return df
 
-    def download_image_from_url(self, url, name, format):
-        rqst = requests.get(url)
+    def download_image_from_url(self, url: str, filepath: str, format: str) -> None:
+        response = requests.get(url)
+        with open(f"{filepath}.{format}", "wb") as file:
+            file.write(response.content)
 
-        with open(f"{name}.{format}", "wb") as file:
-            file.write(rqst.content)
+    def download_image_from_gallery(
+        self, gallery_id: str, filepath: str, format: str
+    ) -> list:
+        try:
+            post = self._check_none_type_submission(gallery_id)
+            if post is not None:
+                if hasattr(post, "media_metadata"):
+                    url_gallery_list = [
+                        i[1]["p"][0]["u"].split("?")[0].replace("preview", "i")
+                        for i in post.media_metadata.items()
+                    ]
 
-    def download_image_from_gallery(self, gallery_id, filepath, format):
-        post = self._test_none_type_submission(gallery_id)
-        if post is None:
-            pass
-        else:
-            url_gallery_list = [
-                i[1]["p"][0]["u"].split("?")[0].replace("preview", "i")
-                for i in post.media_metadata.items()
-            ]
+                    for i, url in enumerate(url_gallery_list):
+                        response = requests.get(url)
+                        with open(f"{filepath}-.{format}", "wb") as file:
+                            file.write(response.content)
 
-            for i, url in enumerate(url_gallery_list):
-                rqst = requests.get(url)
+                    return url_gallery_list
+                else:
+                    logger.error(
+                        f"Submission with ID {gallery_id} does not have media metadata."
+                    )
+            else:
+                logger.error(f"Submission with ID {gallery_id} is None.")
+        except Exception as e:
+            logger.error(
+                f"An error occurred while downloading images from gallery: {e}"
+            )
 
-                with open(f"{filepath}_{i}.{format}", "wb") as file:
-                    file.write(rqst.content)
+    def get_images(self, name_by: str) -> None:
+        """Saves images into the specified direcotry
 
-            return url_gallery_list
+        Args:
+            name_by (str, optional): _description_. Defaults to "id".
+        """
+        try:
+            df = self._get_posts()
+            df_classified = self.classify_urls(df)
+            logger.info(df_classified)
 
-    def get_images(self, name_by="id"):
-        if not os.path.isdir(f"{self.user_name}"):
-            self._make_directory()
-            logger.info(f"Created directory /{self.user_name}")
-        else:
-            logger.error(f"Directory /{self.user_name} already exists")
+            for i, url in enumerate(df_classified["url"]):
+                if name_by == "id":
+                    name_i = df_classified["id"][i]
+                elif name_by == "created_utc":
+                    name_i = datetime.utcfromtimestamp(
+                        df_classified["created_utc"][i]
+                    ).strftime("%Y-%m-%d--%H-%M-%S")
 
-        df = self._get_posts()
-        df_classified = self.classify_urls(df)
-
-        for i, url in enumerate(df_classified["url"]):
-            if name_by == "id":
-                name_i = df_classified["id"][i]
-            elif name_by == "created_utc":
-                name_i = datetime.utcfromtimestamp(
-                    df_classified["created_utc"][i]
-                ).strftime("%Y-%m-%d--%H-%M-%S")
-
-            filepath = f"{self.user_name}/{name_i}"
-            if df_classified["type"][i] == "image":
-                self.download_image_from_url(url, filepath, "jpg")
-            elif df_classified["type"][i] == "gallery":
-                self.download_image_from_gallery(
-                    df_classified["id"][i], filepath, "jpg"
-                )
-            elif df_classified["type"][i] == "unknown":
-                pass
+                filepath = f"{self.user_path}/{name_i}"
+                if df_classified["type"][i] == "image":
+                    self.download_image_from_url(
+                        url=url, filepath=filepath, format=DEFAULT_IMAGE_FORMAT
+                    )
+                elif df_classified["type"][i] == "gallery":
+                    self.download_image_from_gallery(
+                        gallery_id=df_classified["id"][i],
+                        filepath=filepath,
+                        format=DEFAULT_IMAGE_FORMAT,
+                    )
+                elif df_classified["type"][i] == "unknown":
+                    pass
+        except Exception as e:
+            logger.error(f"An error occurred while getting images: {e}")
